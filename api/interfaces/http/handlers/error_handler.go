6f918e292442951f87888d3f028f4f248d87a095
Yokoyama hirokazu <hiroyky.voyager@gmail.com>
2025-04-05 23:22:01 +0900

    feat

--- api/interfaces/http/handlers/error_handler.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/labstack/echo/v4"
-
-func HandleError(err error, ctx echo.Context) {
-
-}
---
+++ api/interfaces/http/routers/photo_handler.go
@@ -0,0 +1,11 @@
+package routers
+
+import "github.com/labstack/echo/v4"
+
+func (h *handler) GetPhotos(ctx echo.Context, params GetPhotosParams) error {
+	panic("")
+}
+
+func (h *handler) GetPhoto(ctx echo.Context, photoId int) error {
+	panic("")
+}
